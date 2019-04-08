adaa0ba1d9b4e220579062efa03a9c800c72225a
karthikrangasai <karthikrangasai@gmail.com>
2019-04-08 23:38:09 +0530

    Added mineBlock(), viewUser and multiple nodes pending

--- flaskApp.py
+++ flaskApp.py
@@ -1,22 +1,10 @@
-from collections import OrderedDict
-
-import binascii
-
 import Crypto
-import Crypto.Random
 from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_v1_5
 
-import hashlib
-import json
 from datetime import datetime as dt
-from urllib.parse import urlparse
-from uuid import uuid4
 
 import requests
 from flask import Flask, jsonify, request, render_template, redirect, url_for, flash
-# from flask_cors import CORS
 
 
 # To be used data for testing
